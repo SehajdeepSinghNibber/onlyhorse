607dabe31ed0438a1a71ba594f7687b0c164b9fe
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-04-25 05:23:46 +0530

    couldinary video that was added is fixed now

--- src/components/auth-screen/TodaysHighlight.tsx
+++ src/components/auth-screen/TodaysHighlight.tsx
@@ -4,12 +4,12 @@ import 'next-cloudinary/dist/cld-video-player.css';
 
 const TodaysHighlight = () => {
   return (
-    <div className='w-full md:wd-3/4 mx-auto'>
+    <div className='w-full md:w-3/4 mx-auto'>
       <CldVideoPlayer 
-      width={"960"}
-      height={"540"}
-      className='rounded-md'
-      src="highlighted-vid_iwai3k"
+        width="480"
+        height="270"
+        className='rounded-md mx-auto'
+        src="highlighted-vid_iwai3k"
       />
     </div>
   )
